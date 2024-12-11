3bdebf94f055119675c4752f1afaebd64d0a49d9
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2024-12-11 16:58:24 +0100

    mathe.py

---
+++ mathe.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+
+print("Addition", a + b)
+print("Subtraktion", a - b)
+print("Multiplikation", a * b)
+print("Division", a / b)
+
+note1 = 3
+note2 = 4
+note3 = 8
+
+print("Durchschnitt", (note1 + note2 + note3) / 3)
