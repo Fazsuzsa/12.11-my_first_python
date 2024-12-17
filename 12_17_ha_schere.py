8c8de431bf89aa350f36499905ab05c7ad13fd78
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2024-12-17 18:02:43 +0100

    Update 12_17_ha_schere.py

--- 12_17_ha_schere.py
+++ 12_17_ha_schere.py
@@ -4,33 +4,40 @@
 def spiel():
     user_choice = input("Wähle Schere, Stein oder Papier! ")
 
+    if not (
+        user_choice == "Schere" or user_choice == "Stein" or user_choice == "Papier"
+    ):
+        print("Ungültige Angabe! Pass auf die kleinen und großen Buchstaben auf!")
+        spiel()
+
     import random
 
     compu_choice = random.choice(["Schere", "Stein", "Papier"])
-
-    print(f"Deine Wahl war {user_choice}, dein Gegner hat {compu_choice} gewählt.")
 
     if (
         (user_choice == "Schere" and compu_choice == "Schere")
         or (user_choice == "Stein" and compu_choice == "Stein")
         or (user_choice == "Papier" and compu_choice == "Papier")
     ):
-        print("Unentschieden.")
+        print(
+            f"Unentschieden. Deine Wahl war {user_choice}, dein Gegner hat {compu_choice} gewählt."
+        )
     elif (
         (user_choice == "Schere" and compu_choice == "Papier")
         or (user_choice == "Stein" and compu_choice == "Schere")
         or (user_choice == "Papier" and compu_choice == "Stein")
     ):
-        print("Glückwunsch! Du hast gewonnen!")
+        print(
+            f"Glückwunsch! Du hast gewonnen! Deine Wahl war {user_choice}, dein Gegner hat {compu_choice} gewählt."
+        )
     elif (
         (user_choice == "Schere" and compu_choice == "Stein")
         or (user_choice == "Stein" and compu_choice == "Papier")
         or (user_choice == "Papier" and compu_choice == "Schere")
     ):
-        print("Du hast leider verloren.")
-    else:
-        print("Ungültige Angabe! Pass auf die kleinen und großen Buchstaben auf!")
-        spiel()
+        print(
+            f"Du hast leider verloren. Deine Wahl war {user_choice}, dein Gegner hat {compu_choice} gewählt."
+        )
 
 
 spiel()
