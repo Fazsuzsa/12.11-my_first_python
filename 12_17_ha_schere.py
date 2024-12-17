d13f72ca05de35823787157d1e494981a788991d
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2024-12-17 17:35:07 +0100

    Update 12_17_ha_schere.py

--- 12_17_ha_schere.py
+++ 12_17_ha_schere.py
@@ -1,7 +1,36 @@
 # Aufgabe 4
 
-user_choice = input("Wähle Schere, Stein oder Papier! ")
 
-import random
+def spiel():
+    user_choice = input("Wähle Schere, Stein oder Papier! ")
 
-print(random.choice(["Schere", "Stein", "Papier"]))
+    import random
+
+    compu_choice = random.choice(["Schere", "Stein", "Papier"])
+
+    print(f"Deine Wahl war {user_choice}, dein Gegner hat {compu_choice} gewählt.")
+
+    if (
+        (user_choice == "Schere" and compu_choice == "Schere")
+        or (user_choice == "Stein" and compu_choice == "Stein")
+        or (user_choice == "Papier" and compu_choice == "Papier")
+    ):
+        print("Unentschieden.")
+    elif (
+        (user_choice == "Schere" and compu_choice == "Papier")
+        or (user_choice == "Stein" and compu_choice == "Schere")
+        or (user_choice == "Papier" and compu_choice == "Stein")
+    ):
+        print("Glückwunsch! Du hast gewonnen!")
+    elif (
+        (user_choice == "Schere" and compu_choice == "Stein")
+        or (user_choice == "Stein" and compu_choice == "Papier")
+        or (user_choice == "Papier" and compu_choice == "Schere")
+    ):
+        print("Du hast leider verloren.")
+    else:
+        print("Ungültige Angabe! Pass auf die kleinen und großen Buchstaben auf!")
+        spiel()
+
+
+spiel()
