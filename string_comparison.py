da2bae270a361d1617c4a4167e8bd0c19653257c
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2024-12-20 17:11:17 +0100

    update

--- string_comparison.py
+++ string_comparison.py
@@ -9,3 +9,7 @@ date_1 = "2024.11.13"
 date_2 = "2024.12.12"
 
 print(f"date_1 < date_2 {date_1 < date_2}")
+
+zahlen = [1, 2, 3, 4, 5]
+print("Die erste Zahl ist:", zahlen[0])
+print("Die letzte Zahl ist:", zahlen[-1])
