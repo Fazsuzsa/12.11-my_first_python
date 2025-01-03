aaa301d8e23efe26604eb51d5788672ec374b866
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-01-03 16:59:33 +0100

    Update 25_01_03_a.py

--- 25_01_03_a.py
+++ 25_01_03_a.py
@@ -34,3 +34,24 @@ if mein_input in deu_eng:
     print(f"Die Übersetzung von {mein_input} lautet: {uebersetz_input}.")
 else:
     print(f"Das Wort {mein_input} befindet sich nicht im Wörterbuch.")
+
+
+# für die Hausaufgabe
+def translate(german_word):
+    my_dict = {"Apfel": "apple", "Wörterbuch": "dictionary"}
+
+    if german_word in my_dict:
+        print(f"Die Übersetzung zu {german_word} ist {my_dict[german_word]}.")
+    else:
+        actual_translation = input(
+            "Das Wort gibt es noch nicht, gib die englische Übersetzung ein: "
+        )
+        my_dict[german_word] = actual_translation
+        print(f"Die Übersetzung zu {german_word} ist {my_dict[german_word]}.")
+
+
+my_userinput = input("Gib ein deutsches Wort ein: ")
+if my_userinput == "exit":
+    print("I am not going to translate anything.")
+else:
+    translate(my_userinput)
