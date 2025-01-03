b8a9ab474f79355b8421fd554c713bf8efb5deb9
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-01-03 10:11:10 +0100

    Update 25_01_02_ha.py

--- 25_01_02_ha.py
+++ 25_01_02_ha.py
@@ -1,12 +1,17 @@
 # Aufgabe 3
+
 zahl_1 = int(input("Gib eine Zahl ein: "))
 zahl_2 = int(input("Gib eine größere Zahl ein: "))
 
 liste = list(range(zahl_1, zahl_2 + 1))
+# print(liste)
 
 
 def ungerade_zahlen():
-    return [x for x in liste if x % 2 != 0]
+    return [a for a in liste if a % 2 != 0]
+
+
+# print(ungerade_zahlen())
 
 
 def summe():
@@ -14,3 +19,21 @@ def summe():
 
 
 print(summe())
+
+
+# andere Lösung (Nassima)
+
+x = int(input("x = "))
+y = int(input("y = "))
+
+y = y + 1
+i = range(x, y)
+
+sum = 0
+z = 0
+for n in i:
+    if n % 2 != 0:
+        sum = sum + int(n)
+        z = z + 1
+
+print(sum)
