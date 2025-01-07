f6ea69927a050730df6ea858d38ea919a75c3ac4
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-01-07 20:54:29 +0100

    Update wetter.py

--- wetter.py
+++ wetter.py
@@ -1,9 +1,13 @@
 import requests
 
-response = requests.get("https://wttr.in/berlin?format=j1")
+# response = requests.get("https://wttr.in/berlin?format=j1")
+# daten = response.json()
+
+input_city = input("Gib eine Stadt ein: ")
+
+response = requests.get(f"https://wttr.in/{input_city}?format=j1")
 daten = response.json()
 
-# print(daten)
 
 temperatur = daten["current_condition"][0]["temp_C"]
 wetter = daten["current_condition"][0]["weatherDesc"][0]["value"]
