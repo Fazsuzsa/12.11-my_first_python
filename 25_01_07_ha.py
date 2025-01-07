8225f56e8f8414947e1cbda4e5422796bf4af574
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-01-07 20:24:43 +0100

    Update 25_01_07_ha.py

--- 25_01_07_ha.py
+++ 25_01_07_ha.py
@@ -1,1 +1,12 @@
 # Aufgabe 1
+class Zutat:
+    def __init__(self, name, kalorien, zeit):
+        self.name = name
+        self.kalorien_pro_100g = kalorien
+        self.zubereitungszeit = zeit
+
+
+class Rezept:
+    def __init__(self, name, beschreibung):
+        self.name = name
+        self.beschreibung = beschreibung
