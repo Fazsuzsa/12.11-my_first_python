f91202b71f4cfd6371552f0987fbb98858b2f09f
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-01-08 23:47:26 +0100

    Update 25_01_08_ha.py

--- 25_01_08_ha.py
+++ 25_01_08_ha.py
@@ -9,7 +9,20 @@ class Haustier:
     def get_description(self):
         print(f"{self.name} ist ein {self.age} Jahre alter {self.species}.")
 
+    def play(self, duration):
+        self.energy_level = self.energy_level - duration * 5
+        if self.energy_level >= 0:
+            print(
+                f"{self.name} hat {duration} Minuten gespielt, der neue Energielevel ist {self.energy_level}."
+            )
+        else:
+            print(
+                f"{self.name} konnte nur {self.energy_level/5} Minuten spielen, der neue Energielevel ist 0."
+            )
 
-mimi = Haustier("Mimi", "Katze", "3", "Fisch", "100")
+
+mimi = Haustier("Mimi", "Katze", "3", "Fisch", 100)
+
 
 mimi.get_description()
+mimi.play(50)
