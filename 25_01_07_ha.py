daffc5e689103c6cd8b8a4bd72b4f6558b636fb9
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-01-09 10:09:29 +0100

    Update 25_01_07_ha.py

--- 25_01_07_ha.py
+++ 25_01_07_ha.py
@@ -11,3 +11,26 @@ class Rezept:
         self.name = name
         self.beschreibung = beschreibung
         self.zutatenliste = {}  # dictionary
+
+    def zutat_hinzufuegen(self, zutat, menge):
+        self.zutatenliste[zutat] = menge
+
+    def kalorien(self):
+        kalorien_counter = 0
+        for zutat in self.zutatenliste:
+            kalorien_counter += zutat.kalorien_pro_100g
+        print(f"Die Gesamtkalorien sind: {kalorien_counter} kcal.")
+
+    def kochzeit(self):
+        kochzeit = 0
+        for zutat in self.zutatenliste:
+            if zutat.zubereitungszeit > kochzeit:
+                kochzeit = zutat.zubereitungszeit
+        print(f"Die Kochzeit beträgt {kochzeit} Minuten.")
+
+    def rezept_anzeigen(self):
+        print(f"Rezept: {self.name}")
+        print("Zutaten: ")
+        for zutat, menge in self.zutatenliste.items():
+            print(f"- {zutat.name}: {menge}")
+        print(f"Beschreibung des Rezepts: {self.beschreibung}")
