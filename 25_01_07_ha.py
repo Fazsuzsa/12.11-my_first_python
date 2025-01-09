c5179f2045851594a873f2f21b28f55155c24575
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-01-09 10:21:21 +0100

    Update 25_01_07_ha.py

--- 25_01_07_ha.py
+++ 25_01_07_ha.py
@@ -34,3 +34,19 @@ class Rezept:
         for zutat, menge in self.zutatenliste.items():
             print(f"- {zutat.name}: {menge}")
         print(f"Beschreibung des Rezepts: {self.beschreibung}")
+
+
+milch = Zutat("Milch", 50, 5)
+eier = Zutat("Eier", 150, 0)
+mehl = Zutat("Mehl", 300, 10)
+zucker = Zutat("Zucker", 400, 5)
+
+pfannkuchen = Rezept("Pfannkuchen", "Super leckere und fluffugen Pfannkuchen!")
+pfannkuchen.zutat_hinzufuegen(milch, "250 ml")
+pfannkuchen.zutat_hinzufuegen(eier, "2 Stück")
+pfannkuchen.zutat_hinzufuegen(mehl, "150 g")
+pfannkuchen.zutat_hinzufuegen(zucker, "50 g")
+
+pfannkuchen.rezept_anzeigen()
+pfannkuchen.kalorien()
+pfannkuchen.kochzeit()
