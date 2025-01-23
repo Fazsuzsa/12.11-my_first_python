6d40f2b927e145f7ced7dfcb239221abcfc3517c
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-01-23 23:24:08 +0100

    Update 25_01_23_ha.py

--- 25_01_23_ha.py
+++ 25_01_23_ha.py
@@ -1,29 +1,29 @@
 # 1
-# name = input("your name: ")
-# age = int(input("your age: "))
-# print(f"{name.capitalize()} will turn 100 years old in the year {(100-age)+2025}.")
+name = input("your name: ")
+age = int(input("your age: "))
+print(f"{name.capitalize()} will turn 100 years old in the year {(100-age)+2025}.")
 
 # 2
-# number = int(input("enter a number: "))
-# if number % 2 == 0:
-#     print(f"{number} is an even number.")
-# else:
-#     print(f"{number} is an odd number.")
+number = int(input("enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
 
 # 3
-# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# for x in a:
-#     if x < 5:
-#         print(x)
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for x in a:
+    if x < 5:
+        print(x)
 
 # 4
-# number = int(input("enter a number: "))
-# list = list(range(1, number + 1))
-# div_list = []
-# for x in list:
-#     if number % x == 0:
-#         div_list.append(x)
-# print(div_list)
+number = int(input("enter a number: "))
+list = list(range(1, number + 1))
+div_list = []
+for x in list:
+    if number % x == 0:
+        div_list.append(x)
+print(div_list)
 
 # 5
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
