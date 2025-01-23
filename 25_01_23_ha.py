58d648b18ea3d188beceab1841c74f52db8c630f
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2025-01-23 23:12:34 +0100

    Update 25_01_23_ha.py

--- 25_01_23_ha.py
+++ 25_01_23_ha.py
@@ -11,3 +11,25 @@
 #     print(f"{number} is an odd number.")
 
 # 3
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# for x in a:
+#     if x < 5:
+#         print(x)
+
+# 4
+# number = int(input("enter a number: "))
+# list = list(range(1, number + 1))
+# div_list = []
+# for x in list:
+#     if number % x == 0:
+#         div_list.append(x)
+# print(div_list)
+
+# 5
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+y = 0
+for x in a:
+    if x == b[y]:
+        print(x)
+        y = y + 1
